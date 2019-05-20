14a376fa0f900566fd72e9c48d3c33af3eb4470c
Arie Brainware <publicu001@protonmail.com>
2019-05-20 12:47:51 +0700

    Add homepage

---
+++ HomePage.php
@@ -0,0 +1,337 @@
+<html>
+<head>
+<title>Travel Web Design</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- Save for Web Slices (Travel Web Design.psd) -->
+<table id="Table_01" width="1401" height="2201" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td colspan="27">
+			<img src="images/Travel-Web-Design_01.jpg" width="1400" height="15" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="15" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6" rowspan="8">
+			<img src="images/Travel-Web-Design_02.jpg" width="511" height="1017" alt=""></td>
+		<td colspan="3">
+			<img src="images/Travel-Web-Design_03.jpg" width="79" height="63" alt=""></td>
+		<td rowspan="29">
+			<img src="images/Travel-Web-Design_04.jpg" width="33" height="2185" alt=""></td>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_05.jpg" width="87" height="63" alt=""></td>
+		<td colspan="2" rowspan="2">
+			<img src="images/Travel-Web-Design_06.jpg" width="40" height="358" alt=""></td>
+		<td colspan="3">
+			<img src="images/Travel-Web-Design_07.jpg" width="80" height="63" alt=""></td>
+		<td colspan="2" rowspan="6">
+			<img src="images/Travel-Web-Design_08.jpg" width="50" height="840" alt=""></td>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_09.jpg" width="121" height="63" alt=""></td>
+		<td colspan="6" rowspan="8">
+			<img src="images/Travel-Web-Design_10.jpg" width="399" height="1017" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="63" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="3" rowspan="5">
+			<img src="images/Travel-Web-Design_11.jpg" width="79" height="777" alt=""></td>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_12.jpg" width="87" height="295" alt=""></td>
+		<td colspan="3">
+			<img src="images/Travel-Web-Design_13.jpg" width="80" height="295" alt=""></td>
+		<td colspan="2" rowspan="7">
+			<img src="images/Travel-Web-Design_14.jpg" width="121" height="954" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="295" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="5">
+			<img src="images/Travel-Web-Design_15.jpg" width="153" height="53" alt=""></td>
+		<td colspan="2" rowspan="4">
+			<img src="images/Travel-Web-Design_16.jpg" width="54" height="482" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="53" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="5">
+			<img src="images/Travel-Web-Design_17.jpg" width="153" height="345" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="345" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="25">
+			<img src="images/Travel-Web-Design_18.jpg" width="52" height="1429" alt=""></td>
+		<td colspan="2">
+			<a href="https://www.facebook.com/profile.php?id=100003484994498"><img src="images/Travel-Web-Design_19.jpg" width="49" height="46" alt=""></a></td>
+		<td colspan="2" rowspan="25">
+			<img src="images/Travel-Web-Design_20.jpg" width="52" height="1429" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="46" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2" rowspan="24">
+			<img src="images/Travel-Web-Design_21.jpg" width="49" height="1383" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="38" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			<img src="images/Travel-Web-Design_22.jpg" width="9" height="177" alt=""></td>
+		<td>
+			<a href="https://www.instagram.com/arie_brainware/"><img src="images/Travel-Web-Design_23.jpg" width="50" height="46" alt=""></a></td>
+		<td rowspan="2">
+			<img src="images/Travel-Web-Design_24.jpg" width="20" height="177" alt=""></td>
+		<td rowspan="23">
+			<img src="images/Travel-Web-Design_25.jpg" width="45" height="1345" alt=""></td>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_26.jpg" width="50" height="46" alt=""></td>
+		<td rowspan="23">
+			<img src="images/Travel-Web-Design_27.jpg" width="9" height="1345" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="46" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/Travel-Web-Design_28.jpg" width="50" height="131" alt=""></td>
+		<td colspan="2" rowspan="22">
+			<img src="images/Travel-Web-Design_29.jpg" width="50" height="1299" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="131" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="21">
+			<img src="images/Travel-Web-Design_30.jpg" width="121" height="1168" alt=""></td>
+		<td colspan="2" rowspan="2">
+			<img src="images/Travel-Web-Design_31.jpg" width="229" height="36" alt=""></td>
+		<td colspan="2" rowspan="3">
+			<img src="images/Travel-Web-Design_32.jpg" width="13" height="50" alt=""></td>
+		<td colspan="4" rowspan="2">
+			<img src="images/Travel-Web-Design_33.jpg" width="227" height="36" alt=""></td>
+		<td colspan="4">
+			<img src="images/Travel-Web-Design_34.jpg" width="229" height="32" alt=""></td>
+		<td colspan="4" rowspan="9">
+			<img src="images/Travel-Web-Design_35.jpg" width="291" height="202" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="32" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="4" rowspan="2">
+			<img src="images/Travel-Web-Design_36.jpg" width="229" height="18" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="4" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_37.jpg" width="229" height="14" alt=""></td>
+		<td colspan="4">
+			<img src="images/Travel-Web-Design_38.jpg" width="227" height="14" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="14" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_39.jpg" width="229" height="36" alt=""></td>
+		<td rowspan="18">
+			<img src="images/Travel-Web-Design_40.jpg" width="11" height="1118" alt=""></td>
+		<td colspan="5">
+			<img src="images/Travel-Web-Design_41.jpg" width="229" height="36" alt=""></td>
+		<td colspan="4">
+			<img src="images/Travel-Web-Design_42.jpg" width="229" height="36" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="36" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_43.jpg" width="229" height="14" alt=""></td>
+		<td colspan="5">
+			<img src="images/Travel-Web-Design_44.jpg" width="229" height="14" alt=""></td>
+		<td colspan="4" rowspan="5">
+			<img src="images/Travel-Web-Design_45.jpg" width="229" height="116" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="14" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_46.jpg" width="229" height="36" alt=""></td>
+		<td rowspan="2">
+			<img src="images/Travel-Web-Design_47.jpg" width="2" height="50" alt=""></td>
+		<td colspan="4">
+			<img src="images/Travel-Web-Design_48.jpg" width="227" height="36" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="36" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_49.jpg" width="229" height="14" alt=""></td>
+		<td colspan="4">
+			<img src="images/Travel-Web-Design_50.jpg" width="227" height="14" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="14" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_51.jpg" width="229" height="36" alt=""></td>
+		<td colspan="5">
+			<img src="images/Travel-Web-Design_52.jpg" width="229" height="36" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="36" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_53.jpg" width="229" height="16" alt=""></td>
+		<td colspan="5" rowspan="13">
+			<img src="images/Travel-Web-Design_54.jpg" width="229" height="982" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="16" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/Travel-Web-Design_55.jpg" width="105" height="35" alt=""></td>
+		<td rowspan="12">
+			<img src="images/Travel-Web-Design_56.jpg" width="124" height="966" alt=""></td>
+		<td colspan="3" rowspan="2">
+			<img src="images/Travel-Web-Design_57.jpg" width="125" height="540" alt=""></td>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_58.jpg" width="105" height="35" alt=""></td>
+		<td colspan="3" rowspan="2">
+			<img src="images/Travel-Web-Design_59.jpg" width="290" height="540" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="35" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="11">
+			<img src="images/Travel-Web-Design_60.jpg" width="105" height="931" alt=""></td>
+		<td colspan="2">
+			<img src="images/Travel-Web-Design_61.jpg" width="105" height="505" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="505" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="10">
+			<img src="images/Travel-Web-Design_62.jpg" width="70" height="426" alt=""></td>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_63.jpg" width="358" height="33" alt=""></td>
+		<td rowspan="10">
+			<img src="images/Travel-Web-Design_64.jpg" width="92" height="426" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="33" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_65.jpg" width="358" height="8" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="8" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_66.jpg" width="358" height="32" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="32" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_67.jpg" width="358" height="6" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="6" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_68.jpg" width="358" height="36" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="36" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_69.jpg" width="358" height="6" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="6" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_70.jpg" width="358" height="114" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="114" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="images/Travel-Web-Design_71.jpg" width="358" height="9" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="9" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="5" rowspan="2">
+			<img src="images/Travel-Web-Design_72.jpg" width="253" height="182" alt=""></td>
+		<td>
+			<img src="images/Travel-Web-Design_73.jpg" width="105" height="35" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="35" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/Travel-Web-Design_74.jpg" width="105" height="147" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="147" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="images/spacer.gif" width="121" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="105" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="124" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="11" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="2" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="148" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="9" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="50" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="20" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="33" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="52" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="35" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="14" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="26" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="26" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="45" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="9" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="41" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="9" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="70" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="51" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="4" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="104" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="1" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="93" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="105" height="1" alt=""></td>
+		<td>
+			<img src="images/spacer.gif" width="92" height="1" alt=""></td>
+		<td></td>
+	</tr>
+</table>
+<!-- End Save for Web Slices -->
+</body>
+</html>
